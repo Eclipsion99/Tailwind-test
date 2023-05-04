c1b3ac0f9800ac784dbc6172617b9b0cc6752c9a
Mainak <chatterjeemainak2003@gmail.com>
2023-05-04 23:33:08 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["dist/index.html"],
+  theme: {
+    borderWidth: {
+      DEFAULT: '1px',
+      '0': '0',
+      '2': '2px',
+      '3': '3px',
+      '4': '4px',
+      '6': '6px',
+      '8': '8px',
+      '12': '12px',
+    },
+    spacing: {
+      '1': '8px',
+      '2': '12px',
+      '3': '16px',
+      '4': '24px',
+      '5': '32px',
+      '6': '48px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
